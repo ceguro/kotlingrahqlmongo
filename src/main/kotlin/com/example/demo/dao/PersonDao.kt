a94ded81aa1a3df3179920d836c0d96cc93edd08
ceguro <carlos.gutierrez@globant.com>
2019-11-24 16:14:17 -0300

    fix and add method to save

--- src/main/kotlin/com/example/demo/dao/PersonDao.kt
+++ src/main/kotlin/com/example/demo/dao/PersonDao.kt
@@ -1,5 +1,6 @@
 package com.example.demo.dao
 
+import com.example.demo.data.Person
 import com.example.demo.repository.PersonRepository
 import org.springframework.stereotype.Component
 
@@ -11,5 +12,7 @@ class PersonDao(
     fun getPersonById(id: String) = personRepository.findById(id)
 
     fun getPersonByName(name: String) = personRepository.findByNameLike(name)
+
+    fun createPerson(name: String) = personRepository.save(Person(name = name))
 
 }
---
+++ src/main/kotlin/com/example/demo/graphql/Mutation.kt
@@ -0,0 +1,14 @@
+package com.example.demo.graphql
+
+import com.coxautodev.graphql.tools.GraphQLMutationResolver
+import com.example.demo.dao.PersonDao
+import com.example.demo.graphql.input.PersonInput
+import org.springframework.stereotype.Component
+
+@Component
+class Mutation(
+        private val personDao: PersonDao
+) : GraphQLMutationResolver {
+    fun personCreate(input: PersonInput) =
+            personDao.createPerson(input.name)
+}
